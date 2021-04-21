5139d22821a0f60c2f0039b768e68011e61d63c6
Jody Brzovski <jody.brzovski@gmail.com>
2021-04-21 13:33:12 -0400

    react table is working and sorting using state

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -4,12 +4,57 @@ import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
+import Table from "../components/Table";
 import Alert from "../components/Alert";
+import makeData from '../makeData';
 
 
 function Employee() {
-  const [employee, setEmployee] = useState([]);
+  const [employees, setEmployees] = useState([]);
   // const [userIndex, setUserIndex] = useState(0);
+
+  // const data = React.useMemo(() => makeData(2000), [])
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'name.first',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'name.last',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Phone',
+            accessor: 'cell',
+          },
+          {
+            Header: 'Email',
+            accessor: 'email',
+          },
+          {
+            Header: 'Years Employeed',
+            accessor: 'registered.age',
+          },
+          {
+            Header: 'Location',
+            accessor: 'location.city',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
 
   // When the component mounts, a call will be made to get random users.
   useEffect(() => {
@@ -18,9 +63,8 @@ function Employee() {
 
   function getEmployees () {
     API.getEmployees()
-    .then(employee => {
-      setEmployee(employee);
-
+    .then(result => {
+      setEmployees(result.data.results);
     })
     .catch(err => console.log(err));
 }
@@ -39,8 +83,8 @@ function Employee() {
         </Row>
         <Row>
           <Col size="md-12">
-            <p>employee table goes here</p>
-
+            {console.log('employees = ', employees)}
+            <Table columns={columns} data={employees} />
           </Col>
         </Row>
       </Container>
